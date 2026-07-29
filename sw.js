cd24b81c25050d232e8a5029791553d79aa5e604
rizky-afandi <rizkyafandi89@gmail.com>
2026-07-29 16:51:09 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,6 +3,8 @@ const urlsToCache = [
     './',
     './index.html',
     './manifest.json',
+    './192.png
+    './512.png
     'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.1/cropper.min.css',
